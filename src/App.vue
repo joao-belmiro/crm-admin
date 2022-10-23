f01797b2bfc0222a9edb7ea82b32283ea8f7b2e0
João Carlos Belmiro Gonçalves <joao_belmiro@hotmail.com>
2022-10-23 17:15:53 -0300

    initialize project sass,icons

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,17 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <Layout/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+
+import Layout from './components/Layout.vue'
+export default {
+  components: {
+    Layout
+  }
 }
+</script>
+<style lang="scss">
 
 #nav {
   padding: 30px;
